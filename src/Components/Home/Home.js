66a66d9bb07d02b988aad1fbfc79224f37d201fe
Farjana Islam <nipa.farzana87@gmailcon>
2020-09-16 01:37:29 +0600

    navbar done

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Menu from './Menu';
+
+
+
+
+const Home = () => {
+    return (
+        <div className='container-fluid header-body'>
+            <Menu />
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Components/Home/Menu.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from '../../Logo.png';
+
+const Menu = () => {
+    return (
+        <>
+        <style type="text/css">
+        {`
+        .btn-link {
+          color: black;
+        }
+        .btn-primary {
+            background-color: #f9a51a;
+            border-radius: 5px;
+            border: none;
+            color: black
+        }
+        .btn-primary:hover{
+            background-color: #af7009;
+        }
+        .btn-xxl {
+            padding: 0.4rem 1.5rem;
+        }
+        `}
+      </style>
+        <div className='container menu-bar pt-3'>
+            <div className="row d-flex justify-content-between align-items-center">
+                <div className="logo"><Link to="/home"><img className='img-fluid logo' src={logo} alt="" /></Link></div>
+                <div className="">
+                  <input className='search-input' type="text" name="" id="" placeholder='Search your destination'/>
+                </div>
+                <div><Link className="menu-item" to="/home">Home</Link>
+                    <Link className="menu-item" to="/news">News</Link>
+                    <Link className="menu-item" to="/destination">destination</Link>
+                    <Link className="menu-item" to="/blog">Blog</Link>
+                    <Link className="menu-item" to="/contact">Contact</Link>
+                    <Button variant="primary" size="xxl">Login</Button>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+};
+
+export default Menu;
